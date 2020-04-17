0efb1c69a206555c7ed2c6e45a880d10c17b035d
tomsoulie <t.soulie0@gmail.com>
2020-04-17 18:28:47 +0200

    review

--- HandiPlay/Assets/PlayerBehavior.cs
+++ HandiPlay/Assets/PlayerBehavior.cs
@@ -1,30 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    public Text Count;
 
     public float m_speed = 100f;
     public Rigidbody2D m_rb2D;
-    public int m_CandyCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "candy")
-        {
-            Destroy(collision.gameObject);
-            m_CandyCount = m_CandyCount + 1;
-            Count.text = m_CandyCount.ToString ();
-        }
     }
 
     // Update is called once per frame
