46a0e5c004c959147510251217eb23f8ccbb5f84
Hugofgx9 <hugo.faugeroux@mmibordeaux.com>
2020-04-17 10:22:20 +0200

    debut menu

---
+++ HandiPlay/Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+	public void PlayGame()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+	}
+
+	public void QuitGame () 
+	{
+		Application.Quit();
+	}
+
+	    // Start is called before the first frame update
+    void Start()
+    {
+     	   
+    }
+}
